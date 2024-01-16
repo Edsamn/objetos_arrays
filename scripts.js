87a9817f3f457afcc7b9833e46498687f275d975
Edsamn <eduardomsribas@gmail.comm>
2024-01-16 15:07:26 -0300

    commit_main

---
+++ scripts.js
@@ -0,0 +1,63 @@
+// Vamos criar um programa para gerenciar os animais de zoologico. Para isso considere os itens a baixo :
+// a-) Crie uma lista para armazenar os animais
+// b-) Crie um novo animal com as seguintes propriedades : nome, cor, peso, pais_origem, alimentacao, idade
+// c-) Crie 5 animais a partir de uma cópia do animal do item anterior ;
+// d-) Guarde esses animais na lista de animais ;
+// e-) Mude o nome do terceiro animal e do quarto;
+// f-) Imprima as informacoes do segundo animal da lista.
+// g-) Imprima os dados atualizados do terceiro animal da lista
+let listaDeAnimais = [];
+
+let animal = {
+    nome: 'Frajola',
+    cor: 'preto',
+    peso: '6kg',
+    pais_origem: 'Brasil',
+    alimentação: 'ração',
+    idade: '9'
+}
+
+let animal2 = {...animal}
+
+animal2.nome: ,
+animal.cor: ,
+animal2.peso: ,
+animal2.pais_origem: ,
+animal2.alimentação: ,
+animal2.idade: ,
+
+let animal3 = {...animal}
+
+animal3.nome: ,
+animal3.cor: ,
+animal3.peso: ,
+animal3.pais_origem: ,
+animal3.alimentação: ,
+animal3.idade: ,
+
+let animal4 = {...animal}
+
+animal4.nome: ,
+animal4.cor: ,
+animal4.peso: ,
+animal4.pais_origem: ,
+animal4.alimentação: ,
+animal4.idade: ,
+
+let animal5 = {...animal}
+
+animal5.nome: ,
+animal5.cor: ,
+animal5.peso: ,
+animal5.pais_origem: ,
+animal5.alimentação: ,
+animal5.idade: ,
+
+let animal6 = {...animal}
+
+animal6.nome: ,
+animal6.cor: ,
+animal6.peso: ,
+animal6.pais_origem: ,
+animal6.alimentação: ,
+animal6.idade: ,
